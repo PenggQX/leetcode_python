0a646913f7919d62fbbcae91187815d2c0909489
pineapple <1461101200@qq.com>
2022-07-01 22:29:27 +0800

    day0701

--- day/2022_06/30_质数排列.py
+++ day/2022_06/30_质数排列.py
@@ -15,3 +15,13 @@ class Solution:
         i1 = bisect.bisect_right(prime, n)
         i2 = n - i1
         return Jie(i1) * Jie(i2) % (10 ** 9 + 7)
+
+#         E1  E2  E3  E4  E5  E6
+# # Q1    2   3   2   2   3   3       15
+# # Q2    1   1   6   1   6   1       16
+# # Q3    3   2   3   6   2   6       22
+# # Q4    5   6   5   5   5   4       30
+# # Q5    4   4   4   4   4   5       25
+# # Q6    6   5   1   3   1   2       18
+#
+# Q1 < Q2 < Q6 < Q3 < Q5 < Q4
